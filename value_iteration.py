ecd2d3848f285b56844b428c45c2e6bbf6ba4ef5
HDParks <haydendparkinson@gmail.com>
2019-06-14 13:47:41 -0600

    finishing value iteration

--- value_iteration.py
+++ value_iteration.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import numpy as np
 
 class MDP():
-    def __init__(self, rewards, discount):
+    def __init__(self, states, actions, transitions, rewards, discount):
         """
         Parameters:
             states - ndarray of state names
@@ -11,10 +12,10 @@ class MDP():
         self.S = states
         self.A = actions
         self.transitions = transitions
-        self.rewards = rewards
+        self.R = rewards
         self.discount = discount
 
-def value_iteration(mdp, epsilon):
+def value_iteration(mdp, age, epsilon):
     """
     Parameters:
         mdp - an MDP object with states S, actions A(s), transition model P(s'|s,a), rewards R(s), discount gamma
@@ -24,16 +25,50 @@ def value_iteration(mdp, epsilon):
     """
 
     # Initial utility map sends all states to 0
-    U = np.zeros(len(self.S))
+    U = {s:0 for s in mdp.S}
     delta = 0
+    U_next = U.copy()
     while(True):
         for state in mdp.S:
-            U_next[state] = mdp.R(state) + mdp.discount * max([sum([mdp.transitions[action,state,s_next] * U[s_next] for s_next in mdp.S]) for action in mdp.A )
+
+            U_next[state] = mdp.R(state) + mdp.discount * get_neighbor_util(state,age,mdp,U)
 
             if abs(U_next[state] - U[state]) > delta:
                 delta = abs(U_next[state] - U[state])
 
-        if delta < epsilon * (1 - mdp.gamma) / mdp.gamma:
+        print(delta)
+        print(U)
+        U = U_next
+        if delta < epsilon * (1 - mdp.discount) / mdp.discount:
             break
 
+
     return U
+
+def get_neighbor_util(state,age_, mdp,U):
+    u_per_action = np.zeros(len(mdp.A))
+    for i, a_ in enumerate(mdp.A):
+        total = 0
+        for s in mdp.S:
+            u = U[s] * float(mdp.transitions.query("health == @s & action == @a_ & age == @age_ ")[state])
+            total += u
+        u_per_action[i] = total
+    return max(u_per_action)
+
+def prob1():
+    data = pd.read_csv('transition1.csv')
+    # Define states, actions by slicing 'health', 'actions'
+    states = data.health.unique()
+    actions = data.action.unique()
+    age = 30
+    def reward(state):
+        if state == "Dead":
+            print("YoU DeAd")
+            return -100
+        if state == 'noAAA':
+            return 100 - age
+        return .9 * (100 - age)
+
+    # Build the transition matrix using slices of the data
+    mdp = MDP(states, actions, data, reward, .9)
+    return value_iteration(mdp, age, .1)
